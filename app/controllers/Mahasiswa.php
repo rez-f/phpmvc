e834eb728ef01243e1a6b9ddef0f0b2794cb1b31
br14nsmith <moehamadrezha@gmail.com>
2018-12-02 19:16:12 +0700

    Add Flash Message and Delete
    Add flash message and delete data from database function

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -19,6 +19,30 @@ class Mahasiswa extends Controller{
         $this->view('templates/footer');
     }
 
+    public function tambah()
+    {
+        if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+            Flasher::setFlash('berhasil','ditambahkan','success');
+            header('Location: '. BASEURL .'/mahasiswa');
+            exit;
+        }else{
+            Flasher::setFlash('gagal','ditambahkan','danger');
+            header('Location: '. BASEURL .'/mahasiswa');
+        }
+    }
+    
+    public function hapus($id)
+    {
+        if ($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0) {
+            Flasher::setFlash('berhasil','dihapus','success');
+            header('Location: '. BASEURL .'/mahasiswa');
+            exit;
+        }else{
+            Flasher::setFlash('gagal','dihapus','danger');
+            header('Location: '. BASEURL .'/mahasiswa');
+            exit;
+        }
+    }
 }
 
 ?>
--- app/core/Database.php
+++ app/core/Database.php
@@ -65,6 +65,11 @@ class Database{
         $this->execute();
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
 }
 
 ?>
